d446d09ca2535dfe9813611c72e5c046c78777f0
Paul Visciano <paulvisciano.dev@gmail.com>
2026-03-18 17:03:24 +0700

    Bump to v2.8.3 (larger transcript + stacked controls)

--- apps/JARVIS/app.js
+++ apps/JARVIS/app.js
@@ -1,7 +1,7 @@
 // JARVIS Voice Recorder UI - extracted from index.html
 
 // Client version (bumped when UI changes ship)
-const CLIENT_VERSION = '2.8.2';
+const CLIENT_VERSION = '2.8.3';
 const CLIENT_BUILD_DATE = '2026-03-18';
 
 function toggleTranscriptPath() {
